9c5d4b3f562560455b410fb4225b1eb27715b714
Colan <cworstellprogramming@gmail.com>
2023-05-09 02:16:45 -0500

    Fixed 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,7 +4,8 @@
 
 def matrix_shape(matrix):
     """Returns the Shape of 2D and 3D Matrices"""
-    try:
-        return "[{}, {}, {}]".format(len(matrix), len(matrix[0]), len(matrix[0][0]))
-    except Exception:
+    if len(matrix[0]) == 2:
         return "[{}, {}]".format(len(matrix), len(matrix[0]))
+    elif len(matrix[0]) == 3:
+        return "[{}, {}, {}]".format(len(matrix), len(matrix[0]), len(matrix[0][0]))
+
