b4731da23128c4dad544de52a20580453d346473
Colan <cworstellprogramming@gmail.com>
2023-05-09 02:20:25 -0500

    Checker Error

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,8 +4,9 @@
 
 def matrix_shape(matrix):
     """Returns the Shape of 2D and 3D Matrices"""
-    if len(matrix[0]) == 2:
+    if len(matrix) == 1:
+        return matrix
+    elif len(matrix[0]) == 2:
         return "[{}, {}]".format(len(matrix), len(matrix[0]))
     elif len(matrix[0]) == 3:
         return "[{}, {}, {}]".format(len(matrix), len(matrix[0]), len(matrix[0][0]))
-    return None
