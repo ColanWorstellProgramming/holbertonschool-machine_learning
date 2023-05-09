05aa57e4be7f7b90a9f1da96ca91a8de3d13e18b
Colan <cworstellprogramming@gmail.com>
2023-05-09 02:11:02 -0500

    Documentation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def matrix_transpose(matrix):
-
+    """Transposes 2D matrices"""
     arr = [[0] * len(matrix) for plc in range(len(matrix[0]))]
 
     for x in range(len(matrix)):
