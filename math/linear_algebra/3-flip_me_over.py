637d19de7075f95374cd92762702f2f70de3bf05
Colan <cworstellprogramming@gmail.com>
2023-05-09 02:09:24 -0500

    Flip Me Over

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def matrix_transpose(matrix):
+
+    arr = [[0] * len(matrix) for plc in range(len(matrix[0]))]
+
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
+            arr[y][x] = matrix[x][y]
+
+    return arr
