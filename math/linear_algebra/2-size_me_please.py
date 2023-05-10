fa883060d0f220e711ef4a9b1b7cec44b876a577
Colan <cworstellprogramming@gmail.com>
2023-05-09 19:53:10 -0500

    Chris Is Helping Me

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,9 +4,8 @@
 
 def matrix_shape(matrix):
     """Returns the Shape of 2D and 3D Matrices"""
-    if isinstance(matrix, (list, tuple)) and not any(isinstance(elem, (list, tuple)) for elem in matrix):
-        return [1]
-    elif len(matrix[0]) == 2:
-        return "[{}, {}]".format(len(matrix), len(matrix[0]))
-    elif len(matrix[0]) == 3:
-        return "[{}, {}, {}]".format(len(matrix), len(matrix[0]), len(matrix[0][0]))
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
