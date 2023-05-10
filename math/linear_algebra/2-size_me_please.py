b8db91fc21105c2921af7eef1c406602315f17f4
Colan <cworstellprogramming@gmail.com>
2023-05-09 19:50:36 -0500

    Edge

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,7 +5,7 @@
 def matrix_shape(matrix):
     """Returns the Shape of 2D and 3D Matrices"""
     if isinstance(matrix, (list, tuple)) and not any(isinstance(elem, (list, tuple)) for elem in matrix):
-        return None
+        pass
     elif len(matrix[0]) == 2:
         return "[{}, {}]".format(len(matrix), len(matrix[0]))
     elif len(matrix[0]) == 3:
---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Line Up Function"""
+
+
+def add_arrays(arr1, arr2):
+    """Add An Array Function"""
+    if len(arr1) == len(arr2):
+        arr3 = arr1 + arr2
+        return arr3
+    else:
+        return None
