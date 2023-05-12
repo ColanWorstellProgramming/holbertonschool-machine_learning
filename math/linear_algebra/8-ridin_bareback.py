9f83683ecc42a11b074e2a7b06195a30192eec9d
Colan <cworstellprogramming@gmail.com>
2023-05-12 01:56:15 -0500

    2d multiplication

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Bare Back?"""
+
+
+def mat_mul(mat1, mat2):
+    """Multiply amoung the stars"""
+
+    mat3 = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range (len(mat2)):
+                mat3[i][j] += mat1[i][k] * mat2[k][j]
+
+    return mat3
