a788b2cd9cd81bb805308af9df41c7bc76b72b2d
Colan <cworstellprogramming@gmail.com>
2023-05-12 01:59:45 -0500

    2d multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,9 +7,12 @@ def mat_mul(mat1, mat2):
 
     mat3 = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
 
+    if (len(mat1[0]) != len(mat2)):
+        return None
+
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
-            for k in range (len(mat2)):
+            for k in range(len(mat2)):
                 mat3[i][j] += mat1[i][k] * mat2[k][j]
 
     return mat3
