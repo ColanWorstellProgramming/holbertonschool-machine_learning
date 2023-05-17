6cd1fbede32bd7e0f2cece26c32072652dd44f02
Colan <cworstellprogramming@gmail.com>
2023-05-17 15:23:38 -0500

    Correction

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,22 +2,8 @@
 """Sigma Equation"""
 
 
-#def summation_i_squared(n):
-#    """Addition With Sigma"""
-#    if n >= 1:
-#        return recursive_summation(n, 1, 0)
-#    else:
-#        return None
-#
-#
-#def recursive_summation(n, x, i):
-#    """helper function"""
-#    if x <= n:
-#        i = i + x ** 2
-#        return recursive_summation(n, x + 1, i)
-#    else:
-#        return i
 def summation_i_squared(n):
+    """Addition With Sigma"""
     if n>=1:
         return int((n*(n+1)*(2*n+1))/6)
     else:
