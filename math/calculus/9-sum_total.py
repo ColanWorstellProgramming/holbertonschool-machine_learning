9dea2c7365888dddf6ca8abdfdcfe21f5624377a
Colan <cworstellprogramming@gmail.com>
2023-05-17 15:24:19 -0500

    Correction

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """Addition With Sigma"""
-    if n>=1:
-        return int((n*(n+1)*(2*n+1))/6)
+    if n >= 1:
+        return int((n * (n + 1) * (2 * n + 1)) / 6)
     else:
         return None
