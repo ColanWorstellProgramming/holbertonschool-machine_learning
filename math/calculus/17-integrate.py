fd2d89c2ecea81268c6653c9bc6a75107d25c127
Colan <cworstellprogramming@gmail.com>
2023-05-17 19:57:15 -0500

    Poppin Off

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,11 +6,11 @@ def poly_integral(poly, C=0):
     """poly integral, actually pretty fun but hurts jsut a little"""
     if not isinstance(poly, list):
         return None
-    if len(poly) == 0:
+    if not all(isinstance(i, (int, float)) for i in poly):
         return None
     if not isinstance(C, int):
         return None
-    if not all(isinstance(i, (int, float)) for i in poly):
+    if len(poly) == 0:
         return None
 
     ret = [C]
@@ -22,5 +22,8 @@ def poly_integral(poly, C=0):
             y = int(y)
 
         ret.append(y)
+
+    while len(ret) > 1 and ret[-1] == 0:
+        ret.pop()
 
     return ret
