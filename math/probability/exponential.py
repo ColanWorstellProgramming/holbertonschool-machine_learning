c0427aed05e0888f400d929b7ced78c7e690d5ec
Colan <cworstellprogramming@gmail.com>
2023-05-24 17:49:20 -0500

    expo

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -6,19 +6,18 @@ pi = 3.1415926536
 
 
 class Exponential:
-    """Exponential Class"""
+    """expo class"""
     def __init__(self, data=None, lambtha=1.):
-        """Constructor"""
+        """initialization"""
         self. lambtha = float(lambtha)
         self.data = data
         if data is None:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
-            else:
-                self.lambtha = float(lambtha)
-        else:
-            if not isinstance(data, list):
+        if data is not None:
+            if type(data) != list:
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            self.lambtha = lambtha / sum(data) / len(data)
+
+            self.lambtha = 1. / (sum(data) / len(data))
