8faeb405b368f7040f1024cbf6790ba2f3689e44
Colan <cworstellprogramming@gmail.com>
2023-05-24 18:32:44 -0500

    x and z

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -22,9 +22,18 @@ class Normal:
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
+
             self.mean = sum(data) / len(data)
 
             n = 0
             for x in data:
                 n += (x - self.mean) ** 2
             self.stddev = ((n / len(data)) ** 0.5)
+
+    def z_score(self, x):
+        """z_score"""
+        return ((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        """x_value"""
+        return ((self.stddev * z) + self.mean)
