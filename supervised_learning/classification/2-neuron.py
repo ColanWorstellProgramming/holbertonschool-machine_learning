0fa0fc634fa376f050c0a2350322f952e32e6fce
Colan <cworstellprogramming@gmail.com>
2023-06-02 14:49:25 -0500

    Update

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+    def __init__(self, nx):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        self.__A = self.sigmoid(np.dot(self.__W, X))
+        return self.__A
+
+    def sigmoid(self, X):
+        return 1 / (1 + np.exp(-X))
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
