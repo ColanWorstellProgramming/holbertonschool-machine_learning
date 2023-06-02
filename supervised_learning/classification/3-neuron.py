6c389ba7c85f5822691cea34fc2713bbd1f873b9
Colan <cworstellprogramming@gmail.com>
2023-06-02 16:59:02 -0500

    Cost Function

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+    def __init__(self, nx):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """Forward Propogation"""
+        self.__A = self.sigmoid(np.matmul(self.__W, X) + self.__b)
+        return self.__A
+
+    def sigmoid(self, X):
+        """Sigmoid Helper"""
+        return 1 / (1 + np.exp(-X))
+
+    def cost(self, Y, A):
+        """Cost Func"""
+        m = Y.shape[1]
+        return ((-1/m) * np.sum(Y * np.log(A) + (1.0000001 - Y) * np.log(1.0000001 - A)))
+
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
