d4409fdd2a2c98da0876a1495cf571b5418c48ca
Colan <cworstellprogramming@gmail.com>
2023-06-05 17:19:06 -0500

    Working on 16, not done

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Neural Network Class"""
+    def __init__(self, nx, layers):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, int):
+            raise TypeError("layers must be an integer")
+        if layers < 1:
+            raise ValueError("layers must be a positive integer")
+
+        self.L = layers
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
