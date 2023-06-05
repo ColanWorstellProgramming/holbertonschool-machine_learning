6afe4052dd4fd9ae6f7c09640a67d22c7c34fd8c
Colan <cworstellprogramming@gmail.com>
2023-06-05 13:54:00 -0500

    Test

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Neural Network Class"""
+    def __init__(self, nx, nodes):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nx < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.randn(1, nx)
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nx)
+        self.b2 = 0
+        self.A2 = 0
