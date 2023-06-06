d5f828da4ca79da4dbcedf9adf9dece9e073ca29
Colan <cworstellprogramming@gmail.com>
2023-06-06 15:14:17 -0500

    Decode

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,4 +5,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """Decode Fun"""
+    if type(one_hot) is not np.ndarray:
+        return None
+    if one_hot.ndim != 2:
+        return None
     return np.argmax(one_hot, axis=0)
