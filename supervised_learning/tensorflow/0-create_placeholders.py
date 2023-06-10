a86840307a4fc2848db7ecc4b4e44d67c8b880b1
Colan <cworstellprogramming@gmail.com>
2023-06-09 22:32:13 -0500

    Test

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import numpy as np
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """Create Placeholders"""
+    x = tf.Variable(nx, tf.float32)
+    y = tf.Variable(classes, tf.float32)
+    return x, y
