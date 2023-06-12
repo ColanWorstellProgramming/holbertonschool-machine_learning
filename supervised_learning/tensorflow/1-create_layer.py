a9df2aad72b51799d3a344946d862f5dcac62c87
Colan <cworstelljobs@gmail.com>
2023-06-11 20:40:31 -0700

    He et al

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,5 +7,5 @@ def create_layer(prev, n, activation):
     """Create Layer"""
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init, name='layer')
+                            kernel_initializer=init)
     return layer
