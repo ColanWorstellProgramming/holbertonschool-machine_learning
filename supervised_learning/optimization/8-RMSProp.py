8f22666a0c4e517745f904bcee8f99927283e5ac
Colan <cworstelljobs@gmail.com>
2023-06-17 20:53:59 -0700

    pycode

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -5,4 +5,6 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """Create Variables RMSProp"""
-    return tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon).minimize(loss)
+    return tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                    decay=beta2,
+                                    epsilon=epsilon).minimize(loss)
