39c299ae002431e514ea31c968d45c1eab92f664
Colan <cworstelljobs@gmail.com>
2023-06-17 20:48:37 -0700

    it didnt upload the file?

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Imports"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """Create Variables RMSProp"""
+    return tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon).minimize(loss)
