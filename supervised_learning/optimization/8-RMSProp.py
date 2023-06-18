ea60499b077689713d2a56080b6da6db6bc777c7
Colan <cworstelljobs@gmail.com>
2023-06-17 20:47:02 -0700

    tf not np

--- supervised_learning/optimization/8-RMSProp.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Imports"""
-import numpy as np
-
-
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """Create Variables RMSProp"""
-    return tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon).minimize(loss)
