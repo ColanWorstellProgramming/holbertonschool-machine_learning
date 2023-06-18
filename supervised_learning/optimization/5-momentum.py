9f2c048da80e18c7eb52c8afb031d0a6019818c9
Colan <cworstelljobs@gmail.com>
2023-06-17 20:25:44 -0700

    momentum

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-
+    """Update Variables Momentum"""
     m = beta1 * v + (grad * (1 - beta1))
     v = var - (alpha * m)
 
