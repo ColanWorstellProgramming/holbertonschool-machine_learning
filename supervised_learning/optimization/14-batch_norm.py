bb8391fdd658c76fab9e9af899d6c0cc7b990d47
Colan <cworstelljobs@gmail.com>
2023-06-20 11:00:04 -0700

    batch

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -3,9 +3,6 @@
 import tensorflow as tf
 
 
-def batch_norm(Z, gamma, beta, epsilon):
+def create_batch_norm_layer(prev, n, activation):
     """Batch Normilization Upgrade"""
-    mean = np.mean(Z, axis=0)
-    std = np.std(Z, axis=0)
-    z = ((Z - mean) / ((std ** 2 + epsilon) ** (1 / 2)))
-    return (gamma * z) + beta
+    
