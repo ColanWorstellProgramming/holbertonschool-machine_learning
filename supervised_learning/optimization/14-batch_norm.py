60934129e86e90af2f767ca77e0c5763bc1c90f9
Colan <cworstelljobs@gmail.com>
2023-06-20 15:53:26 -0700

    Batch Normalization with TF

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -5,4 +5,17 @@ import tensorflow as tf
 
 def create_batch_norm_layer(prev, n, activation):
     """Batch Normilization Upgrade"""
-    
+    cont = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    den_lay = tf.layers.Dense(inputs=prev, units=n, kernel_initializer=cont)
+
+    z = dense(prev)
+    mean, var = tf.nn.moments(z, axes=[0])
+
+    gamma = tf.Variable(tf.ones([n]))
+    beta = tf.Variable(tf.zeros([n]))
+
+    Z = tf.nn.batch_normalization(
+        Z, mean, var, offset=beta, scale=gamma, variance_epsilon=1/100000000
+    )
+
+    return activation(Z)
