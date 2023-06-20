5667024d5d8522ff5f6a3ea459e03250fb124c61
Colan <cworstelljobs@gmail.com>
2023-06-20 09:57:53 -0700

    wrong code

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """Update Variables For Adam"""
-    return tf.train.RMSPropOptimizer(learning_rate=alpha,
-                                     decay=beta2,
-                                     epsilon=epsilon).minimize(loss)
+    v = ((beta1 * v) + ((1 - beta1) * grad))
+    s = ((beta2 * s) + ((1 - beta2) * grad ** 2))
+    newV = (v / (1 - beta1 ** t))
+    newS = (s / (1 - beta2 ** t))
+    var = var - alpha * newV / (np.sqrt(newS) + epsilon)
+
+    return var, v, s
