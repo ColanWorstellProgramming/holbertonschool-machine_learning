b6e34cde497cc5295da60df0058f98aab26efa32
Colan Worstell <cworstellprogramming@gmail.com>
2023-06-30 00:45:31 -0500

    why doesnt mine work

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -8,10 +8,10 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Using Tensorflow, This function will create a layer
     that uses L2 regularization.
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     reg = tf.contrib.layers.l2_regularizer(lambtha)
-    layer = tf.layers.Dense(n, activation=activation,
-                            kernal_initializer=init,
-                            kernel_regularizer=reg)
-
-    return layer(prev)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    l2_regularized_layer = tf.layers.Dense(
+        n, activation=activation,
+        kernel_regularizer=reg, kernel_initializer=init
+    )
+    return l2_regularized_layer(prev)
