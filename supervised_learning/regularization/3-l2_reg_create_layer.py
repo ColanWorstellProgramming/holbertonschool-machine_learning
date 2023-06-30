599c4a8efc0fa30881e25efed7bf147359369c58
Colan Worstell <cworstellprogramming@gmail.com>
2023-06-30 00:42:43 -0500

    var names???

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -8,10 +8,10 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Using Tensorflow, This function will create a layer
     that uses L2 regularization.
     """
-    Given = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    R = tf.contrib.layers.l2_regularizer(lambtha)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
     layer = tf.layers.Dense(n, activation=activation,
-                            kernal_initializer=Given,
-                            kernel_regularizer=R)
+                            kernal_initializer=init,
+                            kernel_regularizer=reg)
 
     return layer(prev)
