729978e6a752c8068409fd7f94aeccc7267f3fdd
Colan Worstell <cworstellprogramming@gmail.com>
2023-06-30 00:56:40 -0500

    TF Layer, no meaningless errors please

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Imports"""
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """
+    Using Tensorflow, This function will create a layer
+    that uses Dropout regularization.
+    """
+    reg = tf.layers.Dropout(rate=keep_prob)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(
+        inputs=prev,
+        units=n,
+        activation=activation,
+        kernel_regularizer=reg,
+        kernel_initializer=init
+    )
+    return layer
