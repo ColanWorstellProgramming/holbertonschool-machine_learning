5ee30a71cc5ce377165236cef6e1825d44108c6e
Colan Worstell <cworstellprogramming@gmail.com>
2023-06-30 00:58:17 -0500

    Dense was caps

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -10,7 +10,7 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     """
     reg = tf.layers.Dropout(rate=keep_prob)
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(
+    layer = tf.layers.dense(
         inputs=prev,
         units=n,
         activation=activation,
