ab3f0edddb6c81fe3e996aee24af1fd9e9e511f6
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-04 22:47:15 -0500

    pycode

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -33,7 +33,7 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
         ph, pw = padding
 
     images = np.pad(images, ((0, 0), (ph, ph),
-                             (pw, pw), (0, 0)), mode='constant')
+                             (pw, pw), (0, 0)))
 
     oh = (h + 2 * ph - kh) // sh + 1
     ow = (w + 2 * pw - kw) // sw + 1
@@ -43,7 +43,8 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     for i in range(oh):
         for j in range(ow):
             for k in range(nc):
-                img = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+                img = images[:, i * sh:i * sh + kh,
+                             j * sw:j * sw + kw, :]
                 K = kernels[:, :, :, k]
                 CONVO[:, i, j, k] = np.sum(img * K, axis=(1, 2, 3))
 
