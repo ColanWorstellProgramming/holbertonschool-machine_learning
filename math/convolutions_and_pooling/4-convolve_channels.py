cae263f6eb2a48b3a1d96b059583ff878e33d632
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-04 22:42:01 -0500

    imgs

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -30,7 +30,8 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     else:
         ph, pw = padding
 
-    PADDED = np.pad(images, ((0, 0), (ph, ph), (pw, pw), ((0, 0))), mode='constant')
+    PADDED = np.pad(images, ((0, 0), (ph, ph),
+                             (pw, pw), ((0, 0))), mode='constant')
 
     oh = (h + 2 * ph - kh) // sh + 1
     ow = (w + 2 * pw - kw) // sw + 1
@@ -39,6 +40,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
 
     for i in range(oh):
         for j in range(ow):
-            CONVO[:, i, j] = np.sum(PADDED[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel, axis=(1, 2, 3))
+            CONVO[:, i, j] = np.sum(images[:,i*sh:i*sh+kh,
+                                           j*sw:j*sw+kw] * kernel, axis=(1, 2, 3))
 
     return CONVO
