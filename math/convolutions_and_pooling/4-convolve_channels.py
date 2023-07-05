c84a2aa337d6f08ede1299155193f54c697f9532
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-04 22:39:36 -0500

    spacing ?

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -39,7 +39,6 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
 
     for i in range(oh):
         for j in range(ow):
-            img = PADDED[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
-            CONVO[:, i, j] = np.sum(img * kernel, axis=(1, 2, 3))
+            CONVO[:, i, j] = np.sum(PADDED[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel, axis=(1, 2, 3))
 
     return CONVO
