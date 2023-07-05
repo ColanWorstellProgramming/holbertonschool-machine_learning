4175555b1a7700a05778b5f4ebf5c57ac6e48586
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-04 22:30:40 -0500

    3 im so confused

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -39,7 +39,7 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     for i in range(oh):
         for j in range(ow):
-            img = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
-            CONVO[:, i, j] = np.sum(img * kernel, axis=(1, 2))
+            CONVO[:, i, j] = np.sum(images[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
+                                    * kernel, axis=(1, 2))
 
     return CONVO
