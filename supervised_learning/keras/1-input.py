1b0f59e5a6a3e37d04e192baaf3c940101205de6
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-05 20:39:37 -0500

    1

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Imports"""
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Sequential Modle
+
+    A Sequential model is appropriate for a plain stack of
+    layers where each layer has exactly one input tensor
+    and one output tensor.
+    """
+
+    model = K.Sequential()
+
+    model.add(K.layers.Dense(layers[0],
+                             activation=activations[0],
+                             kernel_regularizer=K.regularizers.l2(lambtha),
+                             input_shape=(nx,)))
+
+    for i in range(1, len(layers)):
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(layers[i],
+                                 activation=activations[i],
+                                 kernel_regularizer=K.regularizers.l2(lambtha)))
+
+    return model
