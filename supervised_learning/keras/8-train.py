48e645558fa64b9944f6b745c05d1cac61942cd5
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-05 22:08:24 -0500

    adding a save for best iteration

---
+++ supervised_learning/keras/8-train.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Imports"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, save_best=False,
+                filepath=None, verbose=True,
+                shuffle=False):
+    """
+    Training the model using keras
+
+    This time we are saving the best iteration of the file
+    """
+    list = []
+
+    if validation_data and early_stopping:
+        list.append(K.callbacks.EarlyStopping(monitor='val_loss',
+                                              mode='min',
+                                              atience=patience))
+
+    if validation_data and learning_rate_decay:
+        def scheduler(epoch):
+            """Scheduler"""
+            return (alpha / (1 + decay_rate * epoch))
+        list.append(K.callbacks.LearningRateScheduler(scheduler,
+                                                      verbose=1))
+
+    if save_best:
+        list.append(K.callbacks.ModelCheckpoint(filepath=filepath,
+                                                save_best_only=True))
+
+    return network.fit(x=data,
+                       y=labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data,
+                       callbacks=list)
