00ea00972869af74d6c1b6abe0a43c45c2ce9118
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-12 16:01:29 -0500

    1.12

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -13,54 +13,47 @@ def lenet5(x, y):
     operation that utilizes Adam optimization, the loss
     tensor, and the accuracy tensor for evaluating the network.
     """
-    pycode = tf.contrib.layers.variance_scaling_initializer()
-    soft = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output)
 
-    conv1 = tf.layers.conv2d(inputs=x,
-                             filters=6,
+    pycode = tf.contrib.layers.variance_scaling_initializer()
+
+    conv1 = tf.layers.Conv2D(filters=6,
                              kernel_size=(5, 5),
                              padding='same',
                              activation=tf.nn.relu,
-                             kernel_initializer=pycode)
+                             kernel_initializer=pycode)(x)
 
-    pool1 = tf.layers.max_pooling2d(inputs=conv1,
-                                    pool_size=(2, 2),
-                                    strides=(2, 2))
+    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                   strides=(2, 2))(conv1)
 
-    conv2 = tf.layers.conv2d(inputs=pool1,
-                             filters=16,
+    conv2 = tf.layers.Conv2D(filters=16,
                              kernel_size=(5, 5),
                              padding='valid',
                              activation=tf.nn.relu,
-                             kernel_initializer=pycode)
+                             kernel_initializer=pycode)(pool1)
 
-    pool2 = tf.layers.max_pooling2d(inputs=conv2,
-                                    pool_size=(2, 2),
-                                    strides=(2, 2))
+    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                   strides=(2, 2))(conv2)
 
-    flatten = tf.layers.flatten(pool2)
+    flatten = tf.layers.Flatten()(pool2)
 
-    fc1 = tf.layers.dense(inputs=flatten,
-                          units=120,
+    fc1 = tf.layers.Dense(units=120,
                           activation=tf.nn.relu,
-                          kernel_initializer=pycode)
+                          kernel_initializer=pycode)(flatten)
 
-    fc2 = tf.layers.dense(inputs=fc1, units=84,
+    fc2 = tf.layers.Dense(units=84,
                           activation=tf.nn.relu,
-                          kernel_initializer=pycode)
+                          kernel_initializer=pycode)(fc1)
 
-    output = tf.layers.dense(inputs=fc2,
-                             units=10,
-                             activation=tf.nn.softmax)
+    y_pred = tf.layers.Dense(units=10,
+                             kernel_initializer=pycode)(fc2)
 
-    loss = tf.reduce_mean(soft)
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
 
-    correct_prediction = tf.equal(tf.argmax(output, 1),
-                                  tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction,
-                                      tf.float32))
+    train_op = tf.train.AdamOptimizer().minimize(loss)
 
-    optimizer = tf.train.AdamOptimizer()
-    train_op = optimizer.minimize(loss)
+    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y_pred, 1),
+                                          tf.argmax(y, 1)), tf.float32))
 
-    return output, train_op, loss, accuracy
+    output = tf.nn.softmax(y_pred)
+
+    return output, train_op, loss, acc
