1e1ba40cb53c8931919546cf07daa8017820511c
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-19 14:14:14 -0500

    pycode

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -14,8 +14,12 @@ def transition_layer(X, nb_filters, compression):
     X = K.layers.BatchNormalization()(X)
     X = K.layers.ReLU()(X)
 
-    X = K.layers.Conv2D(nb_filters, (1, 1), strides=(1, 1), padding='same', kernel_initializer=he_normal)(X)
+    X = K.layers.Conv2D(nb_filters,
+                        (1, 1), strides=(1, 1),
+                        padding='same',
+                        kernel_initializer=he_normal)(X)
 
-    X = K.layers.AveragePooling2D((2, 2), strides=(2, 2))(X)
+    X = K.layers.AveragePooling2D((2, 2),
+                                  strides=(2, 2))(X)
 
     return X, nb_filters
