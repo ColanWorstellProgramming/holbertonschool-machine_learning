e598066a35a54fe0444264392bc97aa6033611da
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-19 14:19:36 -0500

    Unsure what went wrong

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -9,30 +9,23 @@ def dense_block(X, nb_filters, growth_rate, layers):
     """
     he_normal = K.initializers.he_normal()
 
-    concat_layers = [X]
-    nb_filters_concat = nb_filters
-
     for _ in range(layers):
-        X = K.layers.BatchNormalization()(X)
-        X = K.layers.ReLU()(X)
-        X = K.layers.Conv2D(4 * growth_rate,
-                                       (1, 1),
-                                       strides=(1, 1),
-                                       padding='same',
-                                       kernel_initializer=he_normal)(X)
+        x = K.layers.BatchNormalization()(X)
+        x = K.layers.ReLU()(x)
+        x = K.layers.Conv2D(4 * growth_rate,
+                            (1, 1),
+                            padding='same',
+                            kernel_initializer=he_normal)(x)
 
-        X = K.layers.BatchNormalization()(X)
-        X = K.layers.ReLU()(X)
-        X = K.layers.Conv2D(growth_rate,
-                                 (3, 3),
-                                 strides=(1, 1),
-                                 padding='same',
-                                 kernel_initializer=he_normal)(X)
+        x = K.layers.BatchNormalization()(x)
+        x = K.layers.ReLU()(x)
+        x = K.layers.Conv2D(growth_rate,
+                            (3, 3),
+                            padding='same',
+                            kernel_initializer=he_normal)(x)
 
-        concat_layers.append(X)
-        X = K.layers.concatenate(concat_layers,
-                                 axis=-1)
+        X = K.layers.Concatenate()([X, x])
 
-        nb_filters_concat += growth_rate
+        nb_filters += growth_rate
 
-    return X, nb_filters_concat
+    return X, nb_filters
