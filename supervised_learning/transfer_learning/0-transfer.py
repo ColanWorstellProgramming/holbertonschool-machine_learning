441aebf86fb3a194d3e91c0624b32f868b353974
Colan Worstell <cworstellprogramming@gmail.com>
2023-07-26 23:25:05 -0500

    final

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -4,7 +4,6 @@ import numpy as np
 import tensorflow.keras as K
 import tensorflow as tf
 
-
 def preprocess_data(X, Y):
     """Preprocess Data"""
     X_p = X.astype('float32') / 255.0
@@ -20,14 +19,18 @@ def main():
 
     tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
-    pre_trained_model = K.applications.VGG16(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+    base_model = K.applications.MobileNetV2(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
 
-    for layer in pre_trained_model.layers:
+    unfreeze_layers = 100
+    for layer in base_model.layers[:-unfreeze_layers]:
         layer.trainable = False
+    for layer in base_model.layers[-unfreeze_layers:]:
+        layer.trainable = True
 
     model = K.Sequential([
-        pre_trained_model,
-        K.layers.Flatten(),
+        K.layers.Lambda(lambda x: tf.image.resize(x, (128, 128))),
+        base_model,
+        K.layers.GlobalAveragePooling2D(),
         K.layers.Dense(256, activation='relu'),
         K.layers.Dropout(0.5),
         K.layers.Dense(10, activation='softmax')
@@ -35,27 +38,27 @@ def main():
 
     model.compile(loss='categorical_crossentropy', optimizer=K.optimizers.Adam(learning_rate=0.001), metrics=['accuracy'])
 
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rotation_range=15,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
         horizontal_flip=True
     )
 
-    datagen.fit(X_train)
+    validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
 
-    num_samples = X_train.shape[0]
-    validation_split = 0.1
-    validation_samples = int(num_samples * validation_split)
-    train_samples = num_samples - validation_samples
+    lr_scheduler = K.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, verbose=1)
+
+    early_stopping = K.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 
     model.fit(
-        datagen.flow(X_train[:train_samples], y_train[:train_samples], batch_size=64),
-        epochs=5,
-        validation_data=datagen.flow(X_train[train_samples:], y_train[train_samples:], batch_size=64),
+        train_datagen.flow(X_train, y_train, batch_size=64),
+        epochs=7,
+        validation_data=validation_datagen.flow(X_test, y_test, batch_size=64),
+        callbacks=[lr_scheduler, early_stopping],
     )
 
-    model.save("cifar10_with_transfer_learning.h5")
+    model.save("cifar10.h5")
 
 if __name__ == "__main__":
     main()
