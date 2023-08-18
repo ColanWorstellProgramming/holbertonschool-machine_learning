4cd9421c1294eb203cd8d8efb85a3c96237381b8
Colan Worstell <cworstellprogramming@gmail.com>
2023-08-17 22:38:25 -0500

    Yolo

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Imports
+"""
+import numpy as np
+import tensorflow.keras as K
+
+
+class Yolo:
+    """
+    Yolo Class
+    """
+    def __init__(self, model_path, classes_path,
+                 class_t, nms_t, anchors):
+        """
+        Class Constructor
+        """
+        self.model = K.models.load_model(model_path)
+
+        with open(classes_path) as file:
+            class_names = file.read()
+
+        self.class_names = class_names.replace("\n", "|").split("|")[:-1]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
---
+++ supervised_learning/object_detection/README.md
@@ -0,0 +1,1 @@
+Object Detection Model Using Yolov3 Algorithm
