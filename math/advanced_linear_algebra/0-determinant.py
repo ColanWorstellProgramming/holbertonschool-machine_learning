01d0595fbdf4a9e1df588067c4aeff0d6a5c3519
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-07 12:49:02 -0500

    Without numpy now

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Imports"""
-import numpy as np
 
 
 def determinant(matrix):
@@ -14,4 +13,22 @@ def determinant(matrix):
     if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
 
-    return np.linalg.det(matrix)
+    if len(matrix) == 0:
+        return 1
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    determ = 0
+    for i in range(len(matrix)):
+        sub = [row[:i] + row[i+1:] for row in matrix[1:]]
+        cofactor = matrix[0][i] * determinant(sub)
+        if i % 2 == 0:
+            determ += cofactor
+        else:
+            determ -= cofactor
+
+    return determ
