9cd18bd9964191cee2e8e7079177d2df8a89514c
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-09 16:22:28 -0500

    check multiple matricies

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -7,14 +7,20 @@ def determinant(matrix):
     Calculate Determinant
     """
 
-    for i in range(len(matrix)):
-        if not isinstance(matrix, list):
-            raise TypeError("matrix must be a list of lists")
-
-        if len(matrix) != len(matrix[0]):
-            raise ValueError("matrix must be a square matrix")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
 
     if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+
+    for i in range(len(matrix)):
+        if not isinstance(matrix[i], list):
+            raise TypeError("matrix must be a list of lists")
+
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a square matrix")
+
+    if len(matrix == 0):
         return 1
 
     if len(matrix) == 1 and len(matrix[0]) == 1:
