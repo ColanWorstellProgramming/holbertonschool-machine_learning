6912fa2965d4de8d13cd64eedf62ce0d7ccb00d6
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-09 17:44:40 -0500

    inverse

---
+++ math/advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""Imports"""
+
+
+def inverse(matrix):
+    """
+    Inveser of a matrix
+    """
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    for i in range(len(matrix)):
+        if not isinstance(matrix[i], list):
+            raise TypeError("matrix must be a list of lists")
+
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if determinant(matrix) == 0:
+        return None
+
+    adj = adjugate(matrix)
+
+    inv = [[x / determinant(matrix) for x in y] for y in adj]
+
+    return inv
+
+
+def adjugate(matrix):
+    """
+    Adjugate of the matrix
+    """
+
+    return [list(i) for i in zip(*cofactor(matrix))]
+
+
+def cofactor(matrix):
+    """
+    Co-factor from minor
+    """
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    for i in range(len(matrix)):
+        if not isinstance(matrix[i], list):
+            raise TypeError("matrix must be a list of lists")
+
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    cofactor = []
+
+    for x in range(len(matrix)):
+        row = []
+        for y in range(len(matrix)):
+            sub_matrix = [row[:y] + row[y + 1:] for
+                          row in (matrix[:x] + matrix[x + 1:])]
+            row.append(((-1) ** (x + y)) * determinant(sub_matrix))
+        cofactor.append(row)
+
+    return cofactor
+
+
+def determinant(matrix):
+    """
+    Calculate Determinant
+    """
+
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+
+    for i in range(len(matrix)):
+        if not isinstance(matrix[i], list):
+            raise TypeError("matrix must be a list of lists")
+
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 0:
+        return 1
+
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    determ = 0
+    for i in range(len(matrix)):
+        sub = [row[:i] + row[i+1:] for row in matrix[1:]]
+        cofactor = matrix[0][i] * determinant(sub)
+        if i % 2 == 0:
+            determ += cofactor
+        else:
+            determ -= cofactor
+
+    return determ
