736748cb3e8ad4ead3c800f12a22e9a8174b57c1
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-10 13:41:07 -0500

    Formating

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -34,17 +34,20 @@ class MultiNormal:
         if not isinstance(x, np.ndarray):
             raise TypeError('x must be a numpy.ndarray')
 
+        d, d2 = x.shape
+
         if len(x.shape) != 2:
             raise ValueError('x must have the shape ({}, 1)'.format(d))
-
-        d, d2 = x.shape
 
         if d != self.cov.shape[0] or d2 != 1:
             raise ValueError('x must have the shape ({}, 1)'.format(d))
 
         x_minus_mean = x - self.mean
-        exponent_term = -0.5 * np.dot(np.dot(x_minus_mean.T, np.linalg.inv(self.cov)), x_minus_mean)
-        denominator = (2 * np.pi) ** (self.d / 2) * np.sqrt(np.linalg.det(self.cov))
+        exponent_term = -0.5 * np.dot(np.dot(x_minus_mean.T,
+                                             np.linalg.inv(self.cov)),
+                                             x_minus_mean)
+        var = np.sqrt(np.linalg.det(self.cov))
+        denominator = (2 * np.pi) ** (self.d / 2) * var
         pdf_value = (1 / denominator) * np.exp(exponent_term)
 
         pdf_value = np.round(pdf_value, 19)
