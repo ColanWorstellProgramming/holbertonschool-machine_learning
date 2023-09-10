e5659d9e43ff18b3fa2240ca2bcd0e0172e61d31
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-10 12:42:22 -0500

    Simpler Solution

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -17,13 +17,6 @@ def correlation(C):
     if d != d2:
         raise ValueError('C must be a 2D square matrix')
 
-    mean = np.mean(C, axis=0, keepdims=True)
-
     std_dev = np.sqrt(np.diag(C))
 
-    # Normally would use np.cov here
-    co = np.dot((C - mean).T, C - mean) / (d - 1)
-
-    co /= np.outer(std_dev, std_dev)
-
-    return co
+    return np.divide(C, np.outer(std_dev, std_dev))
