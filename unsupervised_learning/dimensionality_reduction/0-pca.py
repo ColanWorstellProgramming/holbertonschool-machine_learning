3642103e5fc800a875074e9b27404c4a3d0f06e6
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-24 14:16:41 -0500

    More correct local testing

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -13,9 +13,12 @@ def pca(X, var=0.95):
     n is the number of data points
     d is the number of dimensions in each point
     all dimensions have a mean of 0 across all data points
-    var is the fraction of the variance that the PCA transformation should maintain
-    Returns: the weights matrix, W, that maintains var fraction of X‘s original variance
-    W is a numpy.ndarray of shape (d, nd) where nd is the new dimensionality of the transformed X
+    var is the fraction of the variance
+    that the PCA transformation should maintain
+    Returns: the weights matrix, W, that maintains
+    var fraction of X‘s original variance
+    W is a numpy.ndarray of shape (d, nd) where nd
+    is the new dimensionality of the transformed X
     """
 
     # Perform SVD on the covariance matrix
--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -13,6 +13,19 @@ def pca(X, ndim):
     n is the number of data points
     d is the number of dimensions in each point
     ndim is the new dimensionality of the transformed X
-    Returns: T, a numpy.ndarray of shape (n, ndim) containing the transformed version of X
+    Returns: T, a numpy.ndarray of shape (n, ndim)
+    containing the transformed version of X
     """
-    
+    # Grab Mean
+    mean = np.mean(X, axis=0)
+
+    # Perform SVD on the covariance matrix
+    _, _, Vt = np.linalg.svd(X - mean)
+
+    # Extract the top ndim eigenvectors (principal components) from U
+    W = Vt[:ndim].T
+
+    # Project X onto the reduced dimensionality
+    T = np.dot(X - mean, W)
+
+    return T
