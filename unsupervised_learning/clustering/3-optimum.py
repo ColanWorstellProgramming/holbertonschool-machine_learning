693951938db4a2503aa5e5c209a17fd61788226b
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-24 23:36:41 -0500

    optimum

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -3,10 +3,37 @@
 Imports
 """
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
     Tests for the optimum number of clusters by variance
     """
-    
+    if X.ndim != 2:
+        return None, None
+
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
+
+    if not isinstance(kmax, int) or kmax <= 0:
+        return None, None
+
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+
+    if kmin > kmax:
+        return None, None
+
+    results = []
+    d_vars = []
+
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append((C, clss))
+
+    vars = np.array([variance(X, C) for C, _ in results])
+    d_vars = vars[0] - vars
+
+    return results, d_vars
