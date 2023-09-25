27107a1321072ac811a30a81524795390885406e
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-25 00:05:10 -0500

    checks out of order

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -30,7 +30,7 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if X.ndim != 2:
         return None, None
 
-    if kmax >= kmin:
+    if kmin > kmax:
         return None, None
 
     results = []
