8f91905cbf210f3946aea82a6b1406dd0ad95ce8
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-24 23:14:03 -0500

    variance

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -9,4 +9,15 @@ def variance(X, C):
     """
     Calculates the total intra-cluster variance for a data set
     """
-    
+    if not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray):
+        return None
+    if len(X.shape) != 2 or len(C.shape) != 2:
+        return None
+
+    distances = np.sum((X[:, np.newaxis] - C) ** 2, axis=2)
+
+    nearest_centroid_indices = np.argmin(distances, axis=1)
+
+    var = np.sum(distances[np.arange(len(X)), nearest_centroid_indices])
+
+    return var
