7ae26a144d4602b3f09c70548f6ca066e6e3892d
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-24 19:34:57 -0500

    Init Cluster

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -9,3 +9,16 @@ def initialize(X, k):
     """
     Initializes cluster centroids for K-means
     """
+    if not isinstance(X, np.ndarray):
+        return None
+    if X.shape[0] < k:
+        return None
+
+    min = np.min(X, axis=0)
+    max = np.max(X, axis=0)
+
+    centroids = np.random.uniform(low=min,
+                                  high=max,
+                                  size=(k, X.shape[1]))
+
+    return centroids
