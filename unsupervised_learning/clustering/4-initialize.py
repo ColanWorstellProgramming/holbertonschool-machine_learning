66f92f277ce35290704b5634865a5540cc4b0326
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 18:23:26 -0500

    init 4

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -3,10 +3,25 @@
 Imports
 """
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
     """
     Initializes variables for a Gaussian Mixture Model
     """
-    
+    if not isinstance(X, np.ndarray) or X.ndim != 2 or not isinstance(k, int) or k <= 0:
+        return None, None, None
+
+    _, d = X.shape
+
+    pi = np.full((k,), 1/k)
+
+    m, _ = kmeans(X, k)
+
+    S = np.zeros((k, d, d))
+
+    for i in range(k):
+        S[i] = np.identity(d)
+
+    return pi, m, S
