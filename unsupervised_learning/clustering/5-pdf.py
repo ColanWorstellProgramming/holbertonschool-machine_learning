a2f1d6acd768479fe4c584de155fcf93d8a16b24
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 18:43:56 -0500

    pdf

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -9,4 +9,30 @@ def pdf(X, m, S):
     """
     Calculates the probability density function of a Gaussian distribution
     """
-    
+    if not isinstance(X, np.ndarray) or not isinstance(m, np.ndarray):
+        return None
+
+    if not isinstance(S, np.ndarray):
+        return None
+
+    _, d = X.shape
+
+    if m.shape != (d,) or S.shape != (d, d):
+        return None
+
+    det_S = np.linalg.det(S)
+
+    if det_S == 0:
+        return None
+
+    S_inv = np.linalg.inv(S)
+
+    mahalanobis = np.sum(np.dot((X - m), S_inv) * (X - m), axis=1)
+
+    final = np.exp(-0.5 * mahalanobis)
+
+    P = (1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det_S))) * final
+
+    P = np.maximum(P, 1e-300)
+
+    return P
