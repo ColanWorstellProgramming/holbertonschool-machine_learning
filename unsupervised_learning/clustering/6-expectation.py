5115df33da7782741f633d44fb42032b4a36b1d9
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 19:53:59 -0500

    expectations

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -13,13 +13,13 @@ def expectation(X, pi, m, S):
     if not isinstance(X, np.ndarray) or not len(X.shape) != 2:
         return None, None
 
-    if  not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+    if not isinstance(pi, np.ndarray) or not len(pi.shape) != 1:
         return None, None
 
-    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+    if not isinstance(m, np.ndarray) or not len(m.shape) != 2:
         return None, None
 
-    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+    if not isinstance(S, np.ndarray) or not len(S.shape) != 3:
         return None, None
 
     n, d = X.shape
