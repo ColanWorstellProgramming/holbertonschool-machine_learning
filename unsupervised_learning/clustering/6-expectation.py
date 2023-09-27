cc5fa5d27c30a6d6fe686f2b51253787ff9142cc
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 19:36:02 -0500

    expectations

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -3,10 +3,36 @@
 Imports
 """
 import numpy as np
+pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
     """
     Calculates the expectation step in the EM algorithm for a GMM
     """
-    
+    if not isinstance(X, np.ndarray) or not isinstance(pi, np.ndarray):
+        return None, None
+
+    if not isinstance(m, np.ndarray) or not isinstance(S, np.ndarray):
+        return None, None
+
+    n, d = X.shape
+    k = pi.shape[0]
+
+    if m.shape != (k, d) or S.shape != (k, d, d):
+        return None, None
+
+    pdf_values = np.zeros((k, n))
+    for i in range(k):
+        pdf_values[i] = pdf(X, m[i], S[i])
+
+    numerator = pi[:, np.newaxis] * pdf_values
+
+    denominator = np.sum(numerator, axis=0)
+
+    g = numerator / denominator
+
+    log_likelihood = np.log(denominator)
+    l = np.sum(log_likelihood)
+
+    return g, l
