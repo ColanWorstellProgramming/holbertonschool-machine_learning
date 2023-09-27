75d594e801ce3496f4e7666f1a81e0a1c5f805a6
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 22:11:53 -0500

    11

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -2,11 +2,23 @@
 """
 Imports
 """
-import numpy as np
+import sklearn.mixture
 
 
 def gmm(X, k):
     """
     Calculates a GMM from a dataset
     """
-    
+    gmm = sklearn.mixture.GaussianMixture(n_components=k)
+
+    gmm.fit(X)
+
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+
+    clss = gmm.predict(X)
+
+    bic = gmm.bic(X)
+
+    return pi, m, S, clss, bic
