9b728724133bb5be9ae86ca1579458b1341d5642
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 22:05:32 -0500

    9 check taking forever

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -3,10 +3,13 @@
 Imports
 """
 import numpy as np
+import sklearn.cluster
 
 
 def kmeans(X, k):
     """
     Performs K-means on a dataset
     """
-    
+    centroids, cluster_assignments, _ = sklearn.cluster.k_means(X, k)
+
+    return centroids, cluster_assignments
