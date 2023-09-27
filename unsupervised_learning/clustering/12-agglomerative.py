18270b9e05b0a14d8cfc5fb3f8e34cff9f571b90
Colan Worstell <cworstellprogramming@gmail.com>
2023-09-26 22:21:34 -0500

    12

--- unsupervised_learning/clustering/12-agglomerative.py
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -2,11 +2,20 @@
 """
 Imports
 """
-import numpy as np
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
 
 
 def agglomerative(X, dist):
     """
     Performs agglomerative clustering on a dataset:
     """
-    
+    linkage_matrix = scipy.cluster.hierarchy.linkage(X, method='ward')
+
+    clss = scipy.cluster.hierarchy.fcluster(linkage_matrix, t=dist, criterion='distance')
+
+    scipy.cluster.hierarchy.dendrogram(linkage_matrix, color_threshold=dist)
+
+    plt.show()
+
+    return clss
