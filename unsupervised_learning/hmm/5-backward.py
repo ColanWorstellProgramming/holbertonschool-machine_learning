e6c407782209ee961f4dad5e706081f1e4274fc5
Colan Worstell <cworstellprogramming@gmail.com>
2023-10-01 22:56:13 -0500

    backwards instead this time

--- unsupervised_learning/hmm/5-backward.py
+++ unsupervised_learning/hmm/5-backward.py
@@ -9,4 +9,24 @@ def backward(Observation, Emission, Transition, Initial):
     """
     Performs the backward algorithm for a hidden markov model
     """
-    
+    T = len(Observation)
+    N, _ = Emission.shape
+
+    if Initial.shape[0] != N:
+        return None, None
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    B = np.zeros((N, T))
+
+    B[:, -1] = 1
+
+    for t in range(T - 2, -1, -1):
+        for i in range(N):
+            for j in range(N):
+                B[i, t] += Transition[i, j] * Emission[j, Observation[t + 1]] * B[j, t + 1]
+
+    P = np.sum(Initial[:, 0] * Emission[:, Observation[0]] * B[:, 0])
+
+    return P, B
