43522033c76861fddcfff34f57b0a085d3483b1a
Colan Worstell <cworstellprogramming@gmail.com>
2023-10-01 21:25:52 -0500

    Forward, Checks Passed

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -9,4 +9,25 @@ def forward(Observation, Emission, Transition, Initial):
     """
     Performs the forward algorithm for a hidden markov model
     """
-    
+    T = Observation.shape[0]
+    N, _ = Emission.shape
+
+    if Initial.shape[0] != N:
+        return None, None
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
+        return None, None
+
+    F = np.zeros((N, T))
+
+    for i in range(N):
+        F[i, 0] = Initial[i] * Emission[i, Observation[0]]
+
+    for t in range(1, T):
+        for j in range(N):
+            for i in range(N):
+                F[j, t] += F[i, t - 1] * Transition[i, j] * Emission[j, Observation[t]]
+
+    P = np.sum(F[:, -1])
+
+    return P, F
