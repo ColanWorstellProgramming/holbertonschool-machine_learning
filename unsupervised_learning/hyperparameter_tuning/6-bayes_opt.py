0ef949c63a0982a676ae3b66511c02ea8a725ac6
Colan Worstell <cworstellprogramming@gmail.com>
2023-10-08 01:46:50 -0500

    Template, done for night

---
+++ unsupervised_learning/hyperparameter_tuning/6-bayes_opt.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""
+Imports
+"""
+import GPyOpt
+import numpy as np
+import os
+import tensorflow as tf
+
+
+# Define the objective function to optimize (this should be your model training process)
+def objective_function(parameters):
+    # Extract hyperparameters from the optimization parameters
+    learning_rate = parameters[0]
+    num_units = int(parameters[1])
+    dropout_rate = parameters[2]
+    l2_reg_weight = parameters[3]
+    batch_size = int(parameters[4])
+
+    # Train your machine learning model with the given hyperparameters and return the metric to optimize
+    # Example: Replace this with your model training and evaluation logic
+    metric_to_optimize = train_and_evaluate_model(learning_rate, num_units, dropout_rate, l2_reg_weight, batch_size)
+
+    # Return the metric value to optimize (e.g., validation accuracy, loss, etc.)
+    return metric_to_optimize
+
+# Replace this with your actual model training and evaluation logic
+def train_and_evaluate_model(learning_rate, num_units, dropout_rate, l2_reg_weight, batch_size):
+    # Your model training code here
+    # Return the metric to optimize (e.g., validation accuracy, loss, etc.)
+    return np.random.rand()  # Replace with actual metric value
+
+def save_best_model_checkpoint(best_hyperparameters):
+    # Create a unique identifier for the checkpoint based on hyperparameters
+    checkpoint_name = "model_{}_{}_{}_{}_{}.h5".format(
+        best_hyperparameters[0], best_hyperparameters[1], best_hyperparameters[2],
+        best_hyperparameters[3], best_hyperparameters[4]
+    )
+
+    # Ensure the checkpoint directory exists
+    os.makedirs(checkpoint_dir, exist_ok=True)
+
+    # Create a TensorFlow model using the best hyperparameters (replace with your model architecture)
+    model = build_model(best_hyperparameters)
+
+    # Save the model weights and architecture to a checkpoint file
+    model.save_weights(os.path.join(checkpoint_dir, checkpoint_name))
+
+# Replace this with your actual model architecture
+def build_model(best_hyperparameters):
+    learning_rate, num_units, dropout_rate, l2_reg_weight, batch_size = best_hyperparameters
+
+    # Define your model architecture using the best hyperparameters
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(num_units, activation='relu', input_shape=(input_shape,)))
+    model.add(tf.keras.layers.Dropout(dropout_rate))
+    model.add(tf.keras.layers.Dense(num_units, activation='relu'))
+    model.add(tf.keras.layers.Dropout(dropout_rate))
+    model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+
+    # Compile the model with the best hyperparameters
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+if __name__ == '__main__':
+    # Define the hyperparameter search space
+    domain = [
+        {'name': 'learning_rate', 'type': 'continuous', 'domain': (0.001, 0.1)},
+        {'name': 'num_units', 'type': 'discrete', 'domain': (32, 64, 128)},
+        {'name': 'dropout_rate', 'type': 'continuous', 'domain': (0.0, 0.5)},
+        {'name': 'l2_reg_weight', 'type': 'continuous', 'domain': (1e-6, 1e-3)},
+        {'name': 'batch_size', 'type': 'discrete', 'domain': (16, 32, 64)}
+    ]
+
+    # Create the Bayesian optimization optimizer
+    optimizer = GPyOpt.methods.BayesianOptimization(f=objective_function, domain=domain)
+
+    # Run Bayesian optimization for a maximum of 30 iterations
+    max_iterations = 30
+    optimizer.run_optimization(max_iter=max_iterations)
+
+    # Get the best hyperparameters and the corresponding metric value
+    best_hyperparameters = optimizer.x_opt
+    best_metric = optimizer.fx_opt
+
+    # Print and save the best hyperparameters and metric
+    print("Best Hyperparameters:", best_hyperparameters)
+    print("Best Metric:", best_metric)
+
+    # Save a checkpoint of the best model (you need to implement this)
+    save_best_model_checkpoint(best_hyperparameters)
+
+    # Plot the convergence (optional)
+    optimizer.plot_convergence()
+
+    # Save a report of the optimization to 'bayes_opt.txt'
+    with open('bayes_opt.txt', 'w') as report_file:
+        report_file.write("Best Hyperparameters: {}\n".format(best_hyperparameters))
+        report_file.write("Best Metric: {}\n".format(best_metric))
+        report_file.write(optimizer.get_evaluations())
