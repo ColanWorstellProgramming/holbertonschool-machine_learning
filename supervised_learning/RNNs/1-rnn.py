622b85f12aa1e88eabe5e699058f9321d509d0e6
Colan Worstell <cworstellprogramming@gmail.com>
2023-11-06 21:33:50 -0600

    zeros + 1

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -4,19 +4,26 @@ Imports
 """
 import numpy as np
 
+
 def rnn(rnn_cell, X, h_0):
+    """
+    RNN Forward Prob
+    """
     t, m, _ = X.shape
     h = h_0.shape[1]
 
-    H = np.zeros((t, m, h))
+    H = np.zeros((t + 1, m, h))
     Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
+
+    H[0] = h_0
 
     h_t = h_0
 
     for step in range(t):
+        h_t = H[step]
         x_t = X[step]
         h_t, y_t = rnn_cell.forward(h_t, x_t)
-        H[step] = h_t
+        H[step + 1] = h_t
         Y[step] = y_t
 
     return H, Y
