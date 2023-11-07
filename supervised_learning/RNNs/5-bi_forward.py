d61ac7ed1fc4018a162ad2d5ea05b55a10ed8e36
Colan Worstell <cworstellprogramming@gmail.com>
2023-11-07 13:39:42 -0600

    5

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Imports
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    BidirectionalCell Class
+    """
+    def __init__(self, i, h, o):
+        """
+        Constructor
+        """
+        self.Whf = np.random.randn(h + i, h)
+        self.Whb = np.random.randn(h + i, h)
+        self.Wy = np.random.randn(2 * h, o)
+
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Forward
+        """
+        con = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(con, self.Whf) + self.bhf)
+
+        return h_next
