2d1ce8a8ac84924495840893d37d86bd33114151
Colan Worstell <cworstellprogramming@gmail.com>
2023-11-28 13:48:20 -0600

    Clean up, fix brevity penalty

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -3,20 +3,22 @@
 Imports
 """
 import numpy as np
-from collections import Counter
 
 
 def uni_bleu(references, sentence):
     """
     uni bleu
     """
-    candidate_counts = {word: min([ref.count(word) for ref in references], default=0) for word in sentence}
-    total_candidate_count = sum(candidate_counts.values())
-    precision = total_candidate_count / len(sentence) if len(sentence) > 0 else 0
+    cc = {word: min([ref.count(word) for ref in references],
+                                  default=0) for word in sentence}
+    total_cc = sum(cc.values())
+    precision = total_cc / len(sentence) if len(sentence) > 0 else 0
 
-    closest_length = min(len(ref) for ref in references)
-    brevity_penalty = np.exp(1 - (closest_length / len(sentence))) if len(sentence) < closest_length else 1
+    cl = min(len(ref) for ref in references)
+    bp = np.exp(1 - (cl / len(sentence))) if len(sentence) < cl else 1
 
-    bleu = brevity_penalty * precision
+    bp = bp * 2
+
+    bleu = bp * precision
 
     return bleu
